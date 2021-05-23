ac150c466e876602fedca861362ca9b35761b294
yanagi0306 <82252568+yanagi0306@users.noreply.github.com>
2021-05-23 09:09:07 +0900

    vue3完了

---
+++ src/components/Done.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>コメント</th>
+        <th>状態</th>
+      </tr>
+      <tr v-for="(task, index) in tasks" :key="index">
+        <td v-if="task.status % 2 == 0">{{ index }}</td>
+        <td v-if="task.status % 2 == 0">{{ task.name }}</td>
+        <button v-if="task.status % 2 == 0" v-on:click="statusChange(index)">
+          完了
+        </button>
+        <button v-if="task.status % 2 == 0" v-on:click="del(index)">
+          削除
+        </button>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["tasks"],
+  methods: {
+    statusChange(i) {
+      this.tasks[i].status++;
+    },
+    del(i) {
+      this.tasks.splice(i, 1);
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import All from './components/All.vue'
+import Working from './components/Working.vue'
+import Done from './components/Done.vue'
 
 Vue.config.productionTip = false
+Vue.component('All', All)
+Vue.component('Working', Working)
+Vue.component('Done', Done)
 
 new Vue({
   render: h => h(App),
